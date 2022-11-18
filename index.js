54916761cd68465591271273f2e0a94f6ad93371
LMJayasundara <47645312+LMJayasundara@users.noreply.github.com>
2022-11-18 17:26:11 +0530

    V1.0

---
+++ index.js
@@ -0,0 +1,139 @@
+var express = require('express');
+var app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+app.set('port', (process.env.PORT || 8080));
+let total = 0;
+let maxAmtx = 0;
+
+app.use(express.static('public'));
+
+const BillValidator = require('cashcode-nodejs');
+const device = new BillValidator({
+  baudRate: 19200,
+  // autoPort: true,
+  // boardKeywordIdentifier: 'FTDI'
+  path: "COM3"
+});
+
+function getTotal(cash) {
+  total = total + cash;
+  return total;
+}
+
+(async function () {
+  await device.connect();
+
+  io.on('connection', async (socket) => {
+
+    // device.on('status', (sts)=>{
+    //   socket.emit('Status', sts);
+    // });
+
+    socket.on('maxAmt', (maxAmt)=>{
+      maxAmtx = maxAmt;
+      console.log("maxAmt:", maxAmtx);
+    });
+
+    socket.on('Done', ()=>{
+      total = 0;
+    })
+  });
+
+  device.on('error', (error) => {
+    console.log(error);
+    io.emit('Status', {
+      "error": error
+    });
+  });
+
+  device.on('powerup', function () {
+    io.emit('Status', {
+      "status": 'Device power up'
+    });
+  });
+
+  device.on('reset', function () {
+    io.emit('Status', {
+      "status": 'Device reset'
+    });
+  });
+
+  device.on('initialize', () => {
+    io.emit('Status', {
+      "status": "Device initialize"
+    });
+  });
+
+  device.on("idling", () => {
+    io.emit('Status', {
+      "status": "Device on idle state"
+    });
+  });
+
+  device.on('cassetteRemoved', () => {
+    io.emit('Status', {
+      "status": "Cassette removed"
+    });
+  });
+
+  device.on('cassetteFull', () => {
+    io.emit('Status', {
+      "status": "Cassette full"
+    });
+  });
+
+  device.on('hold', () => {
+    io.emit('Status', {
+      "status": "Device on hold"
+    });
+  });
+
+  device.on('returned', (cash) => {
+    console.log("Returned", cash.amount);
+  });
+
+  device.on('accepting', () => {
+    console.log('accepting');
+  });
+
+  device.on('escrow', async (cash) => {
+    total = getTotal(cash.amount);
+    try {
+      if (total > maxAmtx) {
+        total = total - cash.amount;
+        await device.retrieve();
+      }
+      else {
+        await device.stack();
+        
+        /* Emit current total amount */
+        io.emit('Total', total);
+        /* Emit available refill voulume */
+        io.emit('Refill', total);
+        /* Emit stacked amount */
+        io.emit('Amount', cash.amount);
+      }
+    } catch (error) {
+      io.emit('Status', error.message);
+    }
+  });
+
+  device.on('stacked', (cash) => {
+    try {
+      io.emit('Status', `Stacked ${cash.amount}`);
+    } catch (error) {
+      io.emit('Status', error.message);
+    }
+  });
+
+})();
+
+
+server.listen(app.get('port'), function (err) {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log('Running on port: ' + app.get('port'));
+  }
+});
---
+++ public/script.js
@@ -0,0 +1,143 @@
+const socket = io();
+let timmerevt;
+let defalut_timeout = 10; // Default screen timeout 10s
+
+socket.on('Status', sts => {
+    console.log('Status: ', sts)
+    if(sts.status != null){
+        document.getElementById("error_page").style.display = "none";
+        document.getElementById("status").innerHTML = sts.status;
+    }
+    else{
+        document.getElementById("error_page").style.display = "block";
+        document.getElementById("error_msg").innerHTML = sts.error;
+    }
+
+    document.getElementById("amount").innerHTML = localStorage.getItem("Amount") || 0;
+    document.getElementById("total").innerHTML = localStorage.getItem("Total") || 0;
+    document.getElementById("refill").innerHTML = localStorage.getItem("Refill") || 0;
+
+});
+
+socket.on('Amount', amt => {
+    console.log('Amount: ', amt)
+    localStorage.setItem("Amount", amt);
+});
+
+socket.on('Total', ttl => {
+    console.log('Total: ', ttl);
+    localStorage.setItem("Total", ttl);
+});
+
+socket.on('Refill', refill => {
+    console.log('Refill: ', refill)
+    localStorage.setItem("Refill", refill);
+});
+
+async function fackData(){
+    return new Promise((resolve,reject) => {
+        let amt = 0;
+        let fake_timmer;
+        fake_timmer = setInterval(() => {
+            amt += 1;
+            document.getElementById("fillingAmt").innerHTML = amt;
+
+            if(localStorage.getItem("Total") ==  amt){
+                clearTimeout(fake_timmer);
+                setTimeout(() => {
+                    resolve();
+                }, 3000);
+            }
+        }, 1000);
+    });
+};
+
+/////////////////////////////////////////////////////////////////////////
+// hideAll();
+function hideAll() {
+    document.getElementById("sec1").style.display = "none";
+    document.getElementById("sec2").style.display = "none";
+    document.getElementById("sec3").style.display = "none";
+    document.getElementById("sec4").style.display = "none";
+    document.getElementById("sec5").style.display = "none";
+    document.getElementById("error_page").style.display = "none";
+}
+
+function timmer(timeout, section){
+    clearTimeout(timmerevt);
+    timmerevt = setTimeout(() => {
+        hideAll();
+        document.getElementById(section).style.display = "block";
+    }, timeout * 1000);
+}
+
+function bcktomain(){
+    localStorage.clear();
+    timmer(defalut_timeout, 'sec1');
+}
+
+function cnfsec1(){
+    bcktomain();
+    hideAll();
+    document.getElementById("sec2").style.display = "block";
+}
+
+function cnfsec2(){
+    bcktomain();
+    clearTimeout(timmerevt);
+    let vol = (document.getElementById("inputVol").value);
+    document.getElementById("inputVol").value = '';
+
+    // localStorage.setItem("InputVol", document.getElementById("inputVol").value);
+    // let vol = localStorage.getItem("InputVol");
+
+    if(vol < 10){
+        bcktomain();
+        alert("Enter 10ml at least!")
+    }
+    else{
+        hideAll();
+        document.getElementById("sec3").style.display = "block";
+        socket.emit('maxAmt', vol);
+
+        /* Display max amount in sec2 */
+        document.getElementById("max").innerHTML = vol;
+    }
+}
+
+function cnfsec3(){
+    clearTimeout(timmerevt);
+    hideAll();
+    document.getElementById("sec4").style.display = "block";
+    fackData().then(()=>{
+        cnfsec4();
+    });
+}
+
+function cnfsec4(){
+    bcktomain();
+    hideAll();
+    localStorage.clear();
+    document.getElementById('maxPayment').innerHTML = 0;
+    document.getElementById("sec5").style.display = "block";
+    socket.emit('Done');
+}
+
+/* Calculate max price according to the bottle volume */
+function calprice()
+{
+    var total = document.getElementById('inputVol').value;
+    // var total = localStorage.getItem("InputVol");
+    if(total >= 10){
+        document.getElementById('maxPayment').innerHTML = total;
+    }
+    else{
+        document.getElementById('maxPayment').innerHTML = 0;
+    }
+}
+
+if (typeof(Storage) !== "undefined") {
+    console.log("Code for localStorage/sessionStorage");
+  } else {
+    console.log("Sorry! No Web Storage support..");
+}
